3ea5cb60272b9a729f7cc126a0aa19effeb5dc56
jbengineer <josephablash@gmail.com>
2022-12-27 16:10:43 -0700

    got game to shuffle and deal cards

--- src/App.js
+++ src/App.js
@@ -1,116 +1,18 @@
 
 import "./App.css";
-// // import CardsWrapper from "./components/CardsWrapper";
-
-// // function App() {
-// //   return (
-// //     <div className="App">
-// //       <CardsWrapper cardsNumber="7" />
-// //       <button onClick={() => window.location.reload()}>Reload Cards</button>
-// //     </div>
-// //   );
-// // }
-
-// import JsonDataDisplay from './table'
-// // function App() {
-// //   return (
-// //     <div className="App">
-// //       <h1>Hello Geeks!!!</h1>
-// //       <JsonDataDisplay/>
- 
-// //     </div>
-// //   );
-// // }
-
-// import React, { useState } from 'react';
-
-// function HomePage() {
-//   // state to store the current table index
-//   const [tableIndex, setTableIndex] = useState(0);
-
-//   // array of table data
-//   const tables = [
-//     {
-//       id: 1,
-//       title: 'Table 1',
-//       rows: [
-//         {
-//           id: 1,
-//           cells: ['Cell 1.1', 'Cell 1.2', 'Cell 1.3']
-//         }
-//       ]
-//     },
-//     {
-//       id: 2,
-//       title: 'Table 2',
-//       rows: [
-//         {
-//           id: 1,
-//           cells: ['Cell 2.1', 'Cell 2.2', 'Cell 2.3']
-//         }
-//       ]
-//     },
-//     {
-//       id: 3,
-//       title: 'Table 3',
-//       rows: [
-//         {
-//           id: 1,
-//           cells: ['Cell 3.1', 'Cell 3.2', 'Cell 3.3']
-//         }
-//       ]
-//     }
-//   ];
-
-//   // handler for the button click event
-//   function handleButtonClick() {
-//     // increment the table index and wrap around if it reaches the end
-//     setTableIndex((tableIndex + 1) % tables.length);
-//   }
-
-//   return (
-//     <div className="home-page">
-//       <h1>{tables[tableIndex].title}</h1>
-//       <table>
-//         <thead>
-//           <tr>
-//             {tables[tableIndex].rows[0].cells.map((cell, index) => (
-//               <th style={{ backgroundColor: index % 2 === 0 ? 'lightgreen' : 'green' }}>
-//                 {cell}
-//               </th>
-//             ))}
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <tr>
-//             {tables[tableIndex].rows[0].cells.map((cell, index) => (
-//               <td style={{ backgroundColor: index % 2 === 0 ? 'lightgreen' : 'green' }}>
-//                 {cell}
-//               </td>
-//             ))}
-//           </tr>
-//         </tbody>
-//       </table>
-//       <button style={{ backgroundColor: 'green' }} onClick={handleButtonClick}>
-//         Next Table
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default HomePage;
-
 
 import React, { useState } from 'react';
 
+const deckState = [
+  { suit: '♠️', value: 'A' },{ suit: '♠️', value: 2 },{ suit: '♠️', value: 3 },{ suit: '♠️', value: 4 },{ suit: '♠️', value: 5 },{ suit: '♠️', value: 6 },{ suit: '♠️', value: 7 },{ suit: '♠️', value: 8 },{ suit: '♠️', value: 9 },{ suit: '♠️', value: 10 },{ suit: '♠️', value: 'J' },{ suit: '♠️', value: 'Q' },{ suit: '♠️', value: 'k' },
+  { suit: '♥️', value: 'A' },{ suit: '♥️', value: 2 },{ suit: '♥️', value: 3 },{ suit: '♥️', value: 4 },{ suit: '♥️', value: 5 },{ suit: '♥️', value: 6 },{ suit: '♥️', value: 7 },{ suit: '♥️', value: 8 },{ suit: '♥️', value: 9 },{ suit: '♥️', value: 10 },{ suit: '♥️', value: 'J' },{ suit: '♥️', value: 'Q' },{ suit: '♥️', value: 'k' },
+  { suit: '♦️', value: 'A' },{ suit: '♦️', value: 2 },{ suit: '♦️', value: 3 },{ suit: '♦️', value: 4 },{ suit: '♦️', value: 5 },{ suit: '♦️', value: 6 },{ suit: '♦️', value: 7 },{ suit: '♦️', value: 8 },{ suit: '♦️', value: 9 },{ suit: '♦️', value: 10 },{ suit: '♦️', value: 'J' },{ suit: '♦️', value: 'Q' },{ suit: '♦️', value: 'k' },
+  { suit: '♣️', value: 'A' },{ suit: '♣️', value: 2 },{ suit: '♣️', value: 3 },{ suit: '♣️', value: 4 },{ suit: '♣️', value: 5 },{ suit: '♣️', value: 6 },{ suit: '♣️', value: 7 },{ suit: '♣️', value: 8 },{ suit: '♣️', value: 9 },{ suit: '♣️', value: 10 },{ suit: '♣️', value: 'J' },{ suit: '♣️', value: 'Q' },{ suit: '♣️', value: 'k' },
+]
+
 function DealCard() {
   // state to store the deck of cards
-  const [deck, setDeck] = useState([
-    { suit: '♠️', value: 'A' },{ suit: '♠️', value: 2 },{ suit: '♠️', value: 3 },{ suit: '♠️', value: 4 },{ suit: '♠️', value: 5 },{ suit: '♠️', value: 6 },{ suit: '♠️', value: 7 },{ suit: '♠️', value: 8 },{ suit: '♠️', value: 9 },{ suit: '♠️', value: 10 },{ suit: '♠️', value: 'J' },{ suit: '♠️', value: 'Q' },{ suit: '♠️', value: 'k' },
-    { suit: '♥️', value: 'A' },{ suit: '♥️', value: 2 },{ suit: '♥️', value: 3 },{ suit: '♥️', value: 4 },{ suit: '♥️', value: 5 },{ suit: '♥️', value: 6 },{ suit: '♥️', value: 7 },{ suit: '♥️', value: 8 },{ suit: '♥️', value: 9 },{ suit: '♥️', value: 10 },{ suit: '♥️', value: 'J' },{ suit: '♥️', value: 'Q' },{ suit: '♥️', value: 'k' },
-    { suit: '♦️', value: 'A' },{ suit: '♦️', value: 2 },{ suit: '♦️', value: 3 },{ suit: '♦️', value: 4 },{ suit: '♦️', value: 5 },{ suit: '♦️', value: 6 },{ suit: '♦️', value: 7 },{ suit: '♦️', value: 8 },{ suit: '♦️', value: 9 },{ suit: '♦️', value: 10 },{ suit: '♦️', value: 'J' },{ suit: '♦️', value: 'Q' },{ suit: '♦️', value: 'k' },
-    { suit: '♣️', value: 'A' },{ suit: '♣️', value: 2 },{ suit: '♣️', value: 3 },{ suit: '♣️', value: 4 },{ suit: '♣️', value: 5 },{ suit: '♣️', value: 6 },{ suit: '♣️', value: 7 },{ suit: '♣️', value: 8 },{ suit: '♣️', value: 9 },{ suit: '♣️', value: 10 },{ suit: '♣️', value: 'J' },{ suit: '♣️', value: 'Q' },{ suit: '♣️', value: 'k' },
-  ]);
+  const [deck, setDeck] = useState(deckState);
 
   // state to store the cards that have already been dealt
   const [dealtCards, setDealtCards] = useState([]);
@@ -121,7 +23,6 @@ function DealCard() {
     if (deck.length === 0) {
       return;
     }
-
     // choose a random card from the deck
     const randomIndex = Math.floor(Math.random() * deck.length);
     const card = deck[randomIndex];
@@ -130,17 +31,54 @@ function DealCard() {
     setDeck(deck.filter((_, i) => i !== randomIndex));
     setDealtCards([...dealtCards, card]);
   }
+  // reset the deck and the delt deck.
+  const resetState = () => {
+    setDeck(deckState);
+    setDealtCards([])
+  };
+
+// function Welcome(props) {
+//   var card_color = 'black'
+//   if (props.suit === '♥️' || props.suit === '♦️'){
+//     card_color = 'red'
+//   }
+//   return  <div class="card" style= {card_color} key={prop.index}>{props.value} {props.suit}</div>
+// }
 
   return (
-    <div className="deal-card">
-      <button class="button" onClick={handleDealCard} >Deal Card</button>
-      <div>
-        <strong>Cards Dealt:</strong>
+
+    <div class="container">
+    <div>
+        <div class="row" >
+            <div class="col_top" >
+              <button class="btn btn-primary btn_game" onClick={handleDealCard} type="button" >Deal Card</button>
+              <button class="btn btn-primary btn_game" onClick={resetState} type="button" >Reset Deck</button>
+            </div>
+        </div>
+        <div class="row row_game" >
+            <div class="col text-center"></div>
+            <div class="col text-center">
+                <div>
+                <strong>Cards Dealt:</strong>
         {dealtCards.map((card, index) => (
           <div class="card" style={{ color: card.suit  === '♥️' || card.suit  === '♦️' ? 'red' : 'black' }}key={index}>{card.value} {card.suit}</div>
         ))}
-      </div>
+                </div>
+            </div>
+            <div class="col text-center">
+                <div>
+                <strong>Cards Dealt:</strong>
+                {deck.map((card, index) => (
+                  <div class="card" style={{ color: card.suit  === '♥️' || card.suit  === '♦️' ? 'red' : 'black' }}key={index}>{card.value} {card.suit}</div>
+                ))}
+                </div>
+            </div>
+            <div class="col text-center"></div>
+        </div>
     </div>
+</div>
+
   );
 }
 export default DealCard;
+
